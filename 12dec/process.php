f0eca2696d17f472195afbc80f9daa868bf93f0b
Aamir <ksaak33@gmail.com>
2020-12-19 11:44:14 +0530

    Just trying to sync These Files with students

---
+++ 12dec/process.php
@@ -0,0 +1,11 @@
+<?php
+	echo "POST Fname: ".$_POST['fname'];
+	echo "<br>";
+	echo "POST Gender: ".$_POST['gender'];
+	echo "POST Subject: ".$_POST['subject'];
+
+	// echo "GET Fname: ".$_GET['fname'];
+	// echo "<br>";
+	// echo "GET Lname: ".$_GET['lname'];
+
+?>
