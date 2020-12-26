f9f6c84ef06901f4baeea6e30c210180b02c2946
Aamir <ksaak33@gmail.com>
2020-12-26 18:43:07 +0530

    Intro to js

---
+++ 26dec1/assets/index.js
@@ -0,0 +1,10 @@
+// console.log(document.URL);
+
+// document.write(document.URL);
+// document.writeln("<h1>Hi we are using javascript in our html page.</h1>");
+// mydiv
+
+var div = document.getElementById("mydiv");
+
+console.log(div.innerHTML);
+// div.innerHTML = " This data comes from js";
