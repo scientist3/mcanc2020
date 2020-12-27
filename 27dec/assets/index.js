bf579c34540add7eed90d43964be5e6056589ee4
Aamir <ksaak33@gmail.com>
2020-12-27 12:14:13 +0530

    Js Operator, Function and Control Statements

---
+++ 27dec/assets/index.js
@@ -0,0 +1,102 @@
+// var number1 = 12;
+// var number2 = 30;
+
+// // document.writeln(number1);
+// document.writeln(number1 + number2);
+// document.writeln(number1 - number2);
+// // - + * /
+
+// var x = 5; // Numberic
+// var y = "5"; // String
+
+// var a = 70;
+// var b = 60;
+// var c = 55;
+
+// Program which one is greates
+
+// if (a > b) {
+//   if (a > c) {
+//     console.log("A is >>>>");
+//   } else {
+//     console.log("C is >>>>");
+//   }
+// } else {
+//   if (b > c) {
+//     console.log("B is >>>");
+//   } else {
+//     console.log("C is >>>");
+//   }
+// }
+
+// if (a > b && a > c) {
+//   console.log("A >>>");
+// } else if (b > c) {
+//   console.log("B >>>");
+// } else {
+//   console.log("C >>>");
+// }
+
+// || both cond are false the result is false
+// ! true res false, false = true
+// var fname = "Fazil";
+// var lname = "Ahmad";
+
+// console.log(fname + " " + lname);
+
+// Function Definition
+function hello() {
+  alert("Hi, Im in the function hello");
+}
+
+// Function call
+// hello();
+
+// function findGreatestNumber(a, b, c) {
+//   if (a > b && a > c) {
+//     console.log("A >>>");
+//   } else if (b > c) {
+//     console.log("B >>>");
+//   } else {
+//     console.log("C >>>");
+//   }
+// }
+
+// var A = 9;
+// findGreatestNumber(A, 13, 14);
+
+// var x = 30;
+// var y = 40;
+
+// if (x > y) {
+//   console.log("x>y");
+// }
+
+// if (x > y) {
+//   console.log("Ist statement");
+// } else {
+//   console.log("2nd statement");
+// }
+
+// var num = 2;
+
+// switch (num) {
+//   case 1:
+//     console.log("ONE");
+//     break;
+//   case 2:
+//     console.log("TWO");
+//     break;
+//   case 3:
+//     console.log("THREE");
+//   default:
+//     console.log("Invalid Value");
+//   // break;
+// }
+
+// Looping Structure
+var x = 1;
+while (x < 10) {
+  console.log(x);
+  x += 1;
+}
