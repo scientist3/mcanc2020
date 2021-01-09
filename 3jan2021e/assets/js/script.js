643c07d49af3568cb87f432f255835b39eac2e70
Aamir Bashir <ksaak33@gmail.com>
2021-01-09 12:34:09 +0530

    Jquery & Jquery UI- How it works and few examples

---
+++ 3jan2021e/assets/js/script.js
@@ -0,0 +1,29 @@
+$(function() {  
+  // $( ".draggable" ).draggable();
+  //$( "#accordion" ).accordion(); 
+
+  // var mcaStdNames = [
+  //   "Fazil",
+  //   "Danish",
+  //   "Sadie"
+
+  // ];
+  // $( "#names" ).autocomplete({
+  //   source: mcaStdNames
+  // });
+
+  // $( "button" ).button();
+  $( "#sortable" ).sortable();
+  $( "#sortable" ).disableSelection();
+
+  
+});
+
+
+// $(document).ready(function(){
+// 	document.getElementById('id1')            .innerHTML = "";
+//     $('id1').
+//   $("p").click(function(){
+//     $(this).hide();
+//   });
+// });
