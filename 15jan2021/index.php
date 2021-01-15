fee5a3db5a785cd76799a296463aa7cbaed16845
Aamir Bashir <ksaak33@gmail.com>
2021-01-15 19:20:52 +0530

    jQuery UI Theme and PHP Introduction. Chech notes.

---
+++ 15jan2021/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Php introduction</title>
+</head>
+<body>
+  <h1>Welcome to php.</h1>
+  
+  <?php 
+    echo "<h2>Im in the php code block.</h2>";
+
+  ?>
+
+
+</body>
+</html>
